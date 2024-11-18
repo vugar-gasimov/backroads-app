f6913cf852d8e5df0cc0b7554dcac32f224bd03d
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-11-18 20:48:25 +0400

    New component Title and imroving other component titles, hero img, Footer links. Cleaning all warnings.

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,16 @@
 import React from 'react';
 
+import aboutImg from '../images/about.jpeg';
+
+import Title from './Title';
 const About = () => {
   return (
     <section className='section' id='about'>
-      <div className='section-title'>
-        <h2>
-          about <span>us</span>
-        </h2>
-      </div>
+      <Title title='about' subTitle='us' />
 
       <div className='section-center about-center'>
         <div className='about-img'>
-          <img
-            src='./images/about.jpeg'
-            className='about-photo'
-            alt='awesome beach'
-          />
+          <img src={aboutImg} className='about-photo' alt='awesome beach' />
         </div>
         <article className='about-info'>
           <h3>explore the difference</h3>
@@ -28,7 +23,7 @@ const About = () => {
             Our dedication to excellence means you can relax and focus on
             enjoying your journey while we handle the details seamlessly.
           </p>
-          <a href='#' className='btn'>
+          <a href='/read-more' className='btn'>
             read more
           </a>
         </article>
